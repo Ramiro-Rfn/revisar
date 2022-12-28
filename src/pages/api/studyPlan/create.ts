8f8986e0109bf40e1f8e6b395383b37d28fbdb46
Ramiro Nzau <ramirofrancisco962014@gmail.com>
2022-12-28 11:38:06 +0100

    delete data.ts file

--- src/pages/api/studyPlan/create.ts
+++ src/pages/api/studyPlan/create.ts
@@ -14,6 +14,7 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
         return res.status(201).json(data)
     } catch (error) {
         console.log(error)
+        return res.status(400)
     }
     
 }
